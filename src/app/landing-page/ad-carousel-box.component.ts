242ade1354884595e0a1e8776f1bfd5876d1bfd7
Wong Tze Hsiung <wotzhs@gmail.com>
2016-10-13 10:05:07 +0800

    added carousel images

--- src/app/landing-page/ad-carousel-box.component.ts
+++ src/app/landing-page/ad-carousel-box.component.ts
@@ -7,12 +7,25 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class AdCarouselBoxComponent implements OnInit {
- ngOnInit(){
-  (<any>$('.owl-carousel')).owlCarousel({
-    loop:true,
-    margin:10,
-    items: 1,
-    nav: true,
-  });
- }
+
+  carousel1: string;
+  carousel2: string;
+  carousel3: string;
+  carousel4: string;
+
+  constructor(){
+    this.carousel1 = "../src/assets/img/carousel1.jpg";
+    this.carousel2 = "../src/assets/img/carousel2.png";
+    this.carousel3 = "../src/assets/img/carousel3.jpg";
+    this.carousel4 = "../src/assets/img/carousel4.jpg";
+  }
+
+  ngOnInit(){
+    (<any>$('.owl-carousel')).owlCarousel({
+      loop:true,
+      margin:10,
+      items: 1,
+      nav: true,
+    });
+  }
 }
