cf9936591fd5b3e672a5e634dfe155559460b8bb
Wong Tze Hsiung <wotzhs@gmail.com>
2016-10-20 10:14:03 +0800

    fixed flight tab form field margin

--- src/app/landing-page/booking-box/booking-box-flight-tab.component.ts
+++ src/app/landing-page/booking-box/booking-box-flight-tab.component.ts
@@ -1,12 +1,64 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FlightTypes, BookingChoices, Currencies} from './booking-box';
+import {FlightTypesService, BookingChoicesService, CurrenciesService} from './booking-box.services';
+import {FLIGHTTYPES, BOOKINGCHOICES, CURRENCIES} from '../../mocks/booking-box.mock';
 
 @Component({
   templateUrl: './booking-box-flight-tab.component.html',
-  styleUrls: ['./booking-box-flight-tab.component.css']
+  styleUrls: ['./booking-box-flight-tab.component.css'],
+  providers: [FlightTypesService, BookingChoicesService, CurrenciesService]
 })
 
 export class BookingBoxFlightTabComponent {
 
+  // declaration of types
+  flightTypes: FlightTypes[];
+  bookingChoices: BookingChoices[];
+  currencies: Currencies[];
+  selectedFlightType: string;
+  selectedBookingChoice: string;
+  selectedCurrency: string;
+  childrenInfant: boolean;
+
+  constructor(
+    private flightTypesService: FlightTypesService,
+    private bookingChoicesService: BookingChoicesService,
+    private currenciesService: CurrenciesService
+  ){}
+
+  ngOnInit(){
+    this.getFlightTypes();
+    this.getBookingChoices();
+    this.getCurrencies();
+    this.selectedFlightType = "return";
+    this.childrenInfant = false;
+    this.selectedBookingChoice = "date";
+    this.selectedCurrency = "";
+  }
+
+  getFlightTypes = ():void =>{
+    this.flightTypesService.getFlightTypes().then(flightTypes => this.flightTypes = flightTypes)
+  }
+
+  selectFlightType = (flightType:any)=>{
+    if(flightType !== this.selectedFlightType){
+      this.selectedFlightType = flightType;
+    }
+  }
+
+  selectCurrency = (text:string, code:string)=>{
+    this.selectedCurrency = code + " - " + text
+  }
+
+  getBookingChoices = (): void=>{
+    this.bookingChoicesService.getBookingChoices().then(bookingChoices => this.bookingChoices = bookingChoices)
+  }
+
+  getCurrencies = ():void =>{
+    this.currenciesService.getCurrencies().then(currencies => this.currencies = currencies)
+  }
+
+  // date picker initialization
   myDatePickerOptions = {
   	dateFormat: "dd/mm/yyyy",
   	fistDayOfWeek: 'mo',
@@ -15,21 +67,12 @@ export class BookingBoxFlightTabComponent {
   	inline: false,
   	disableUntil: {year: new Date().getFullYear() , month: new Date().getMonth()+1 , day: new Date().getDate()-1}
   }
-
-  updateDepartDate(event: any){
+  // date picker to set start date
+  updateDepartDate = (event: any) =>{
     console.log('depart date:', event.date, ' - formatted', event.formatted, ' - epoc timestamp', event.epoc);
   }
-
-  updateReturnDate(event: any){
+  // date picker to set start date
+  updateReturnDate = (event: any)=>{
     console.log('return date: ', event.date, ' - formatted', event.formatted, ' - epoc timestamp', event.epoc);
-  }
-
-  setActive = (e:any)=>{
-    (<any>document.querySelectorAll(".ui.circular.red.label")).forEach((i:any)=>{i.classList.remove("red")});
-    e.srcElement.classList.add("red");
-    // alert($.parseHTML(e.srcElement))
-    alert(JSON.stringify(e.target))
-    // alert(JSON.stringify(e.returnValue))
-    // alert(JSON.stringify(e.srcElement))
   }
 }
