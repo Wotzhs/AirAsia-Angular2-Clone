df43943de9b6bc2fa588644dbbb40d56eb2bb07b
Wong Tze Hsiung <wotzhs@gmail.com>
2016-10-25 15:34:19 +0800

    updated datepicker logic to cater/disable backward date selection

--- src/app/landing-page/booking-box/booking-box-hotel-tab.component.ts
+++ src/app/landing-page/booking-box/booking-box-hotel-tab.component.ts
@@ -1,8 +1,66 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {DestinationsService} from './booking-box.services';
+import {Destinations} from './booking-box';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   templateUrl: './booking-box-hotel-tab.component.html',
-  styleUrls: ['./booking-box-hotel-tab.component.css']
+  styleUrls: ['./booking-box-hotel-tab.component.css'],
+  providers: [DestinationsService]
 })
 
-export class BookingBoxHotelTabComponent { }
+export class BookingBoxHotelTabComponent {
+
+  destinations: Destinations[];
+  searchForm : FormGroup;
+  endDateOptions:Object;
+  startDateOptions: Object;
+
+  constructor(
+    private destinationsService : DestinationsService,
+    private formBuilder : FormBuilder
+  ){}
+
+  ngOnInit(){
+    this.destinationsService.getDestinations().then(destinations => this.destinations = destinations)
+    this.searchForm = this.formBuilder.group({
+      destination: ['Destination', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      rooms: 1,
+      adult: 1,
+      children: 0,
+      infant: 0
+    })
+    this.startDateOptions = {
+    	dateFormat: "dd/mm/yyyy",
+    	disableUntil: {year: new Date().getFullYear() , month: new Date().getMonth()+1 , day: new Date().getDate()-1},
+    }
+    this.endDateOptions = {
+      dateFormat: "dd/mm/yyyy",
+      disableUntil: {year: new Date().getFullYear() , month: new Date().getMonth()+1 , day: new Date().getDate()-1}
+    }
+  }
+
+  updateStartDate(event:any){
+    this.searchForm.patchValue({startDate: event.formatted})
+
+    // update disabled date up to the selected date in start date datepicker
+    return this.endDateOptions = {
+    	dateFormat: "dd/mm/yyyy",
+    	disableUntil: event.date
+    }
+  }
+
+  updateEndDate(event:any){
+    this.searchForm.patchValue({endDate: event.formatted})
+
+    // update the disabled date beyond the selected date in end date datepicker for the start date datepicker
+    return this.startDateOptions = {
+    	dateFormat: "dd/mm/yyyy",
+    	disableUntil: {year: new Date().getFullYear() , month: new Date().getMonth()+1 , day: new Date().getDate()-1},
+      disableSince: event.date
+    }
+  }
+
+}
