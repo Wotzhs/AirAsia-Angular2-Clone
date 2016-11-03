db5ed146c1ccba52e6d92623f253a9ded07c37ae
Wong Tze Hsiung <wotzhs@gmail.com>
2016-11-03 12:08:08 +0800

    added logic to process number of children & infant for serializer

--- src/app/landing-page/booking-box/booking-box-hotel-tab.component.ts
+++ src/app/landing-page/booking-box/booking-box-hotel-tab.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core'
 import {DestinationsService} from './booking-box.services';
 import {Destinations} from './booking-box';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {HttpService} from '../../app.service';
+import {Router} from '@angular/router';
 
 @Component({
   templateUrl: './booking-box-hotel-tab.component.html',
@@ -18,7 +20,9 @@ export class BookingBoxHotelTabComponent {
 
   constructor(
     private destinationsService : DestinationsService,
-    private formBuilder : FormBuilder
+    private formBuilder : FormBuilder,
+    private httpService: HttpService,
+    private router: Router
   ){}
 
   ngOnInit(){
@@ -63,18 +67,39 @@ export class BookingBoxHotelTabComponent {
     }
   }
 
+  multiplyString(parameterName:string, item:number[], times:number){
+  	let mapped:string[] = [];
+    if (parameterName === '&children='){
+        for (let i = 0; i < item.reduce((a,b)=>{return a+b}); i++){ // children + infant multiplier
+          for (let j = 0; j < times; j++){ // room multiplier
+            mapped.push(j+1+'_'+ (i < item[0] ? 2 : 1))
+          }
+        }
+    } else {
+      for (let i = 0; i < item[0]; i ++){
+        mapped.push(item[0].toString())
+      }
+    }
+    return parameterName+mapped.sort().join(',')
+  }
+
   serializeData(event:any){
     event.preventDefault();
     const baseUrl= 'https://www.airasiago.com.my/Hotel-Search?';
-    const destination = this.searchForm.value.destination;
-    const startDate = this.searchForm.value.startDate;
-    const endDate = this.searchForm.value.endDate;
+    const destination = 'destination='+this.searchForm.value.destination;
+    const startDate = '&startDate='+this.searchForm.value.startDate;
+    const endDate = '&endDate='+this.searchForm.value.endDate;
     const numberOfRooms = this.searchForm.value.rooms;
-    const numberOfAdult = this.searchForm.value.adult;
-    const numberOfChildren = this.searchForm.value.children;
-    const numberOfInfant = this.searchForm.value.infant;
-    const requestUrl = baseUrl+destination+startDate+endDate+numberOfRooms+numberOfAdult+numberOfChildren+numberOfInfant
+    const numberOfAdult =  this.searchForm.value.adult;
+    const numberOfChildren = eval(this.searchForm.value.children);
+    const numberOfInfant = eval(this.searchForm.value.infant);
+    const requestUrl = baseUrl+destination+startDate+endDate+this.multiplyString('&adults=',[numberOfAdult],numberOfRooms)+this.multiplyString('&children=', [numberOfChildren,numberOfInfant], numberOfRooms)
     return requestUrl
+  }
+
+  setHttpGetData = (data:string)=>{
+    this.httpService.setHttpGetData(data);
+    this.router.navigate(['a']);
   }
 
 }
